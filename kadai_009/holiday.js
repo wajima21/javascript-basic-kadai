b4b13307c561d05dd30bd980560383f721980703
wajima21 <shun.wajima@rakus-partners.co.jp>
2023-08-15 14:39:34 +0900

    説明文追加

--- kadai_009/holiday.js
+++ kadai_009/holiday.js
@@ -1,13 +1,18 @@
 
 let holidays = ["正月","成人の日","建国記念日","天皇誕生日","春分の日","昭和の日","憲法記念日","みどりの日","こどもの日","海の日","山の日","敬老の日","秋分の日","スポーツの日","文化の日","勤労感謝の日"];
 
+// for文
+
 for (let i = 0; i < holidays.length; i++) {
     console.log(holidays[i]);
 }
 
+//while文 
 
 let j = 0;
 while (j < holidays.length){
   console.log(holidays[j]);
   j++;
 }
+
+// length：オブジェクトの「長さ」または「サイズ」を取得するために使用
