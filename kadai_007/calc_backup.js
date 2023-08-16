0e5eaf4a8298908777a5bbc3a830ef7aabc490a5
wajima21 <shun.wajima@rakus-partners.co.jp>
2023-08-16 14:52:38 +0900

    kadai_013

---
+++ kadai_007/calc_backup.js
@@ -0,0 +1,28 @@
+// 変数numに0～99までのランダムな整数を代入する
+let num = Math.floor(Math.random() * 101);
+
+// 確認
+console.log(num)
+
+// 3nの倍数
+
+if (num % 15 === 0 ){
+  console.log('3と5の倍数です');
+}
+
+// 5の倍数
+
+else if (num % 5 === 0){
+  console.log('5の倍数です');
+}
+
+// 3の倍数
+else if (num % 3 === 0){
+  console.log('3の倍数です');
+}
+
+// それ以外
+
+else {
+  console.log(num);
+}
